ae9177f79007e773598358f24e90d485361d5dc6
Pravin <Pravin@Pravin-PC>
2023-02-14 17:54:49 +0530

    added hii

---
+++ GithubPractice/src/github_exam_1/githubexample1.java
@@ -0,0 +1,11 @@
+package github_exam_1;
+
+public class githubexample1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hii");
+		System.out.println("Hallo");
+		System.out.println("how");
+	}
+}
